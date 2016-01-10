a1f50b64f734e63d042d59cf39e36da2e1011455
Mitchell Wolfe <speedywolfe@gmail.com>
2016-01-10 12:50:28 -0700

    add all the stuff that I've been doing

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var items = ["car", "boat", "tree", "house"]
+
+items[0]
+
+items.count
+items.append("frog")
+items.insert("cat", atIndex: 0)
+
+items.removeAtIndex(3)
+
